2e52b59cb501d0936f0259d22ef9dd9ca8c5e75d
Cameron Reikes <cameronreikes@gmail.com>
2019-12-20 11:29:44 -0800

    Add basic gradient lines and update color scheme

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 A local multiplayer cooperation test
 
 ## Color scheme
-https://coolors.co/011627-fdfffc-2ec4b6-e71d36-ff9f1c
+https://coolors.co/011627-fdfffc-6bce39-e71d36-ff9f1c
--- src/PodVisual.gd
+++ src/PodVisual.gd
@@ -2,13 +2,17 @@ tool
 extends Node2D
 
 export var color = Color()
+export var maximum_target_length = 200.0
+export var target_line_length_gradient: Gradient = Gradient.new()
+export var target_offset = Vector2(50, 50)
+export var target_line_width = 5.0
 export var radius = 50.0
-
-func _ready():
-	set_process(Engine.editor_hint)
 
 func _process(delta):
 	update()
 
 func _draw():
+	var tension_amount: float = target_offset.length()/maximum_target_length
+	print(tension_amount)
+	draw_line(Vector2(), target_offset, target_line_length_gradient.interpolate(min(1.0, tension_amount)), target_line_width, true)
 	draw_circle(Vector2(), radius, color)
